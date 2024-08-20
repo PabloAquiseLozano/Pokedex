5f475d8fa2ccbb4af63691a4ba64de8b19670ae5
noelMoriano <nmoriano26@gmail.com>
2024-08-20 10:41:28 -0500

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const btnSearchElement = document.querySelector("#search");
+const namePokemonElement = document.querySelector("#name");
+
 const POKE_API = "https://pokeapi.co/api/v2/";
 
 const fetchPokemonData = async () => {
@@ -6,3 +9,14 @@ const fetchPokemonData = async () => {
 	const results = await Promise.all(data.results);
 	return results;
 };
+
+btnSearchElement.addEventListener('click', async ()=>{
+	const found = await fetchPokemonData();
+	const foundPokemon = found.find(pokemon => pokemon.name.toUpperCase() === namePokemonElement.value.toUpperCase())
+	console.log(foundPokemon);
+	
+	
+	
+	}
+ 
+)
