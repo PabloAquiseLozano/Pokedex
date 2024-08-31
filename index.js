dcad5721a743ffde6e0bd598088d794dfb85c2ab
noelMoriano <nmoriano26@gmail.com>
2024-08-31 11:04:17 -0500

    Mostrar la información del Pokemon con imagen

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 const btnSearchElement = document.querySelector("#search");
 const namePokemonElement = document.querySelector("#name");
+const showPokemon = document.querySelector("#pokeData");
 
-const POKE_API = "https://pokeapi.co/api/v2/";
+const POKE_API = "https://pokeapi.co/api/v2";
 
-const fetchPokemonData = async () => {
-	const response = await fetch(`${POKE_API}/pokemon`);
-	const data = await response.json();
-	const results = await Promise.all(data.results);
-	return results;
+const fetchApi = async (endPoint) => {
+  const response = await fetch(`${POKE_API}/${endPoint}`);
+
+  return await response.json();
 };
 
-btnSearchElement.addEventListener('click', async ()=>{
-	const found = await fetchPokemonData();
-	const foundPokemon = found.find(pokemon => pokemon.name.toUpperCase() === namePokemonElement.value.toUpperCase())
-	console.log(foundPokemon);
-	
-	
-	
-	}
- 
-)
+btnSearchElement.addEventListener("click", async () => {
+  if (namePokemonElement.value.length <= 4) return;
+
+  const pokemon = await fetchApi(
+    `/pokemon/${namePokemonElement.value.toLowerCase()}`
+  );
+
+  const pokeData = `
+  <h2>${pokemon.name.toUpperCase()}</h2>
+  <img src="${pokemon.sprites.front_default}" >
+  `;
+
+  showPokemon.innerHTML = pokeData;
+
+  console.log("pokemon:", pokemon);
+});
+
+//TRAER TODOS LOS POKEMONES
+
+//const fetchPokemonData = async () => {
+//  const data = await fetchApi("pokemon");
+//
+//  const pokemons = data.results.map(async (pokemon) => {
+//    const _pokemon = await fetchApi(`/pokemon/${pokemon.name}`);
+//
+//    return _pokemon;
+//  });
+//
+//  const results = await Promise.all(pokemons);
+//
+//  console.log("results:", results);
+//
+//  return results;
+//};
